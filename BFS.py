7ba9993f36eaed11992cf50c1671bb93c4c99e9a
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-06-02 20:54:27 +0530

    Implement BFS and move zeroes solution
    Added a solution to move zeroes in the list in-place.

--- BFS.py
+++ BFS.py
@@ -43,7 +43,21 @@ dfs_result=g.dfs(start)
 bfs_result=g.bfs(start)
 print("\nDFS traversal:",dfs_result)
 print("\nBFS traversal:",bfs_result)
-
+#problem number two
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        i=0
+        for j in range(n):
+            if nums[j]!=0:
+                nums[i]=nums[j]
+                i=i+1
+        for m in range(i,n):
+            nums[m]=0
         
     
         
