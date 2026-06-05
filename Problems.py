65f91fb8452a5576f73ce9bbc67693784d3a3e63
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-06-05 21:11:42 +0530

    Implement findMaxConsecutiveOnes function
    Added a function to find the maximum number of consecutive ones in a list.

--- Problems.py
+++ Problems.py
@@ -12,3 +12,25 @@ class Solution(object):
                 i=i+1
         for m in range(i,n):
             nums[m]=0
+#problem 2 : problem 485 Maximum consecutive ones
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k=0
+        maxk=0
+        for i in nums:
+            if i:
+                k=k+1
+            else:
+                if k>maxk:
+                    maxk=k
+                k=0
+        if k>maxk:
+            maxk=k
+        return maxk
+        
+        return max(maxk,k)
+        
