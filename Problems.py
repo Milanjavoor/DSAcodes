0756318a87c3cb622dfe8b5fae102c2ddb27bc8b
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-06-14 23:54:02 +0530

    Update Problems.py

--- Problems.py
+++ Problems.py
@@ -90,3 +90,33 @@ class Solution(object):
             if prices[i]<minval:
                 minval=prices[i]
         return maxprof
+    #15. 3Sum
+    class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result=[]
+        for i in range(len(nums)-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                s=nums[i]+nums[l]+nums[r]
+                if s>0:
+                    r-=1
+                elif s<0:
+                    l+=1
+                else:
+                    result.append((nums[i],nums[l],nums[r]))
+                    while l<r and nums[l]==nums[l+1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r-1]:
+                        r-=1
+                    r-=1
+                    l+=1
+                 
+        return result
