979cc2423a326544e0b71235032f2e51caf8322e
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-06-15 23:35:43 +0530

    Update Problems.py

--- Problems.py
+++ Problems.py
@@ -146,3 +146,21 @@ class Solution(object):
                 else:
                     nums[i]=neg[i//2]
         return nums
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n=len(nums)
+        ans=[0]*n
+        pos=0
+        neg=1
+        for i in nums:
+            if i>0:
+                ans[pos]=i
+                pos+=2
+            else:
+                ans[neg]=i
+                neg+=2
+        return ans
