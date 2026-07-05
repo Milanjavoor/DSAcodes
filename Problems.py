f676de98a8b9feeb55e5ae39984ec1c1aa09e53f
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-07-05 23:56:26 +0530

    Update Problems.py

--- Problems.py
+++ Problems.py
@@ -240,4 +240,31 @@ class Solution(object):
                             result.append(new)
                     myset.add(nums[l])
         return result
-       
+
+    def fourSum(self, nums, target):
+        nums.sort()
+        result=[]
+        for i in range(0,len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,len(nums)):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                k=j+1
+                l=len(nums)-1
+                while k<l:
+                    sum=nums[i]+nums[j]+nums[k]+nums[l]
+                    if sum==target:
+                        new=[nums[i],nums[j],nums[k],nums[l]]
+                        result.append(new)
+                        k+=1
+                        l-=1
+                        while k<l and nums[k]==nums[k-1]:
+                            k+=1
+                        while l>k and nums[l]==nums[l+1]:
+                            l-=1
+                    elif sum<target:
+                        k+=1
+                    else:
+                        l-=1
+        return result
