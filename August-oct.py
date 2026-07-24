835b3a8735db3306a6fd70f76dc4a8ce26d15fcd
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-07-24 23:37:23 +0530

    Update August-oct.py

--- August-oct.py
+++ August-oct.py
@@ -1,1 +1,16 @@
-
+46. Permutations
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        final=[]
+        if len(nums)==0:
+            return [[]]
+        perms=self.permute(nums[1:])
+        for p in perms:
+            for i in range(n+1):
+                p_copy=p.copy()
+                p_copy.insert(i,nums[0])
+                if p_copy not in final:
+                    final.append(p_copy)
+        return final
