21acb5afb2b25edc1fdbaab2046bebffbee2cc65
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-07-27 23:33:31 +0530

    Update August-oct.py

--- August-oct.py
+++ August-oct.py
@@ -33,3 +33,6 @@ class Solution:
                 visited[num]=False
         solve()
         return final
+1464. Maximum Product of Two Elements in an Array
+
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1)
