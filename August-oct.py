e0b834c5a783dfdd10d1fa88bca41dd97cf9b495
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-08-29 22:37:55 +0530

    Add binary search method to find peak element

--- August-oct.py
+++ August-oct.py
@@ -157,3 +157,14 @@ class Solution:
                 break
             if nums[i]>nums[i-1] and nums[i]>nums[i+1]:
                 return i
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        lo,hi=0,n-1
+        while hi>lo:
+            mid=(lo+hi)//2
+            if nums[mid]<nums[mid+1]:
+                lo=mid+1
+            else:
+                hi=mid
+        return lo
